b96295c274440ef348b1f519390e24069a48235e
Anushka Gour <gouranushka11@gmail.com>
2024-12-30 21:56:21 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,87 @@
+# a = int(input("Enter a number : "))
+# b = int(input("Enter b number : "))
+# n1 = a 
+# n2 = b 
+# while(b!=0):
+#     temp = b 
+#     b = a%b 
+#     a = temp
+# print(a)    
+#------------------------------------------------------------------------------------
+
+# n = 10
+# for i in range(n):
+#     for j in range(n):
+#         if(i==0 or j==0 or i==n-1 or j==n-1):
+#             print("5",end = " ")
+#         elif(i==1 or j==1 or i==n-2 or j==n-2):
+#             print("4",end = " ")
+#         elif(i==2 or j==2 or i==n-3 or j==n-3):
+#             print("3",end = " ")
+#         elif(i==3 or j==3 or i==n-4 or j==n-4):
+#             print("2",end = " ")
+#         else:
+#             print("1",end = " ")
+#     print()
+
+#-------------------------------------------------------------------------------------------------------------------
+
+# cnt = 65
+# for i in range(1,8):
+#     for j in range(i):
+#         if(i==7 and (j==7 or j==6 or j==5)):
+#             print(chr(128512),end = " ")
+#         else:
+#             print(chr(cnt),end = " ")
+#             cnt= cnt+1 
+#     print() 
+
+#-------------------------------------------------------------------------------------------------------------------
+
+# n = int(input("Enter a number : "))
+# r = 0 
+# while(n!=0):
+#     r = (r*10)+(n%10)
+#     n = n//10
+# print(r)
+
+#-------------------------------------------------------------------------------------
+
+# var = input("Enter a string : ")
+# rev = str()
+# for i in range(len(var)-1,-1,-1):
+#     rev = rev+var[i]
+# print(rev)
+#---------------------------------------------------------------------------------
+# a = int(input("enter a number : "))
+# i = 2 
+# if(a>1):
+#     while(i<a):
+#         if(a%i==0):
+#             print("it is not a prime number : ")
+#             break
+#         i=i+1
+#     else:
+#             print("it is a prime number : ")
+# else:
+#         print("it is not a prime number : ")
+
+#--------------------------------------------------------------------
+#question 1 = assembler
+question 2 =nimonics
+question 3 =intgreted devlopment enviroment
+question 4 =procedur oriented programming and object oriented programming both
+question 5 =l
+question 6 =polymorphism , inheritance , abstraction , encapsu;ation
+question 7 =guido ven rossum , = dob = 20th feb 1991
+question 8 =monty python flying circus
+question 9 = sklearn , transorflow 
+question 10 =python virtual machine
+question 11 = american standard code for  information interchange 
+question 12 =33 keywords in pythone , those who start  in capital letters
+                1. False , 2. True , 3. None
+                
+question 13 = integreted devlopment learning enviroment
+question 14 =
+question 15 = datatypes divided into (6) part  
+
